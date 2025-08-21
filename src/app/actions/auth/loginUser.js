9154022de6e4df3d3a23e4c755d0003ecd25b0d2
Md.Nobin <mdnobin.fdr@gmail.com>
2025-08-21 23:50:38 +0600

    login user actciton all code done

---
+++ src/app/actions/auth/loginUser.js
@@ -0,0 +1,14 @@
+"use server";
+import bcrypt from 'bcrypt'
+import dbConnect, { collectionNamesObj } from "@/lib/dbConnect";
+
+
+export const loginUSer = async (payload) =>{
+    const {email, password} = payload;
+    const usercollection = dbConnect(collectionNamesObj.userCollection);
+    const user = await usercollection.findOne({email})
+    if (!user) return null
+    const isPasswordOK = bcrypt.compare(user.password, password)
+    if(!isPasswordOK) return null
+    return user
+}
