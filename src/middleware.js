0fa3603a6ac443cde9183a22b50bcb43abe17656
Md.Nobin <mdnobin.fdr@gmail.com>
2025-08-26 22:35:17 +0600

    new middlewer added and update code

--- src/middleware.js
+++ src/middleware.js
@@ -28,36 +28,36 @@ export const config = {
 
 
 
-Programming-Hero Instructors
-10:22 PM
-async function onSubmit(data: z.infer<typeof formSchema>) {
-    let redirectPathname = "/";
-    if (typeof window !== "undefined") {
-      redirectPathname = localStorage.getItem("redirectUrl") || "/";
-      localStorage.removeItem("redirectUrl");
-    }
-    setIsLoading(true);
-    const res = await signIn("credentials", {
-      email: data.email,
-      password: data.password,
-      redirect: false,
-      callbackUrl: "/",
-    });
-    setIsLoading(false);
-    if (res?.error) {
-      const error
-= JSON.parse(res.error);
-      // console.log(error, error.statusCode);
-      if (error.statusCode === 403) {
-        setStep("verify-otp");
-        setRegisteredData({ email: data.email, password: data.password });
-        form.reset();
-      }
-      return toast({
-        title: error?.message || "Invalid Credentials!",
-        variant: "destructive",
-      });
-    } else if (res?.status === 200) {
-      return router.replace(redirectPathname);
-    }
-  }
+// Programming-Hero Instructors
+// 10:22 PM
+// async function onSubmit(data: z.infer<typeof formSchema>) {
+//     let redirectPathname = "/";
+//     if (typeof window !== "undefined") {
+//       redirectPathname = localStorage.getItem("redirectUrl") || "/";
+//       localStorage.removeItem("redirectUrl");
+//     }
+//     setIsLoading(true);
+//     const res = await signIn("credentials", {
+//       email: data.email,
+//       password: data.password,
+//       redirect: false,
+//       callbackUrl: "/",
+//     });
+//     setIsLoading(false);
+//     if (res?.error) {
+//       const error
+// = JSON.parse(res.error);
+//       // console.log(error, error.statusCode);
+//       if (error.statusCode === 403) {
+//         setStep("verify-otp");
+//         setRegisteredData({ email: data.email, password: data.password });
+//         form.reset();
+//       }
+//       return toast({
+//         title: error?.message || "Invalid Credentials!",
+//         variant: "destructive",
+//       });
+//     } else if (res?.status === 200) {
+//       return router.replace(redirectPathname);
+//     }
+//   }
